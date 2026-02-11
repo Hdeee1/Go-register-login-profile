eff5162e6b5af89b4e2db24a164bd9fb27c4b817
Hadyu Fiqran Hakim <hadyuhakim@gmail.com>
2026-02-11 14:14:55 +0700

    refactor: enhance user registration flow and error handling, load environment variables

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,38 @@
 package main
 
+import (
+	"fmt"
+	"log"
+
+	"github.com/Hdeee1/go-register-login-profile/internal/delivery/http"
+	repository "github.com/Hdeee1/go-register-login-profile/internal/repository/mysql"
+	"github.com/Hdeee1/go-register-login-profile/internal/usecase"
+	"github.com/Hdeee1/go-register-login-profile/pkg/database"
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+)
+
 func main() {
-	
+	if err := godotenv.Load(".env"); err != nil {
+		log.Fatal("Failed to load env")
+	}
+
+	db, err := database.ConnectMySQL()
+	if err != nil {
+		log.Fatalf("Failed to connect database. Error: %s", err.Error())
+	}
+
+	repo, err := repository.NewUserRepository(db)
+	if err != nil {
+		log.Fatal("Failed to create user repository")
+	}
+
+	useCase := usecase.NewUserUsecase(repo)
+	h := http.NewUserHandler(useCase)
+
+	r := gin.Default()
+	r.POST("/api/user/register", h.Register)
+
+	fmt.Println("Server started at port :8080")
+	r.Run(":8080")
 }
--- pkg/database/mysql.go
+++ pkg/database/mysql.go
@@ -6,6 +6,7 @@ import (
 	"os"
 
 	"github.com/joho/godotenv"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 func ConnectMySQL() (*sql.DB, error) {
